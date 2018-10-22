fd62644f13e359389bcccdfaff2a021f44d9d172
James Abels <abelsj60@gmail.com>
2018-10-21 22:29:57 -0400

    Refactor: MultiProjectNav altered to filter projects for desktop nav only

--- app/MultiProjectNav.jsx
+++ app/MultiProjectNav.jsx
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
 import SingleProjectNav from './SingleProjectNav.jsx';
 import projectData from './data/projectData';
+import { formatProjectName } from './helpers/utils.js';
 
 class MultiProjectNav extends Component {
   constructor(props) {
     super(props);
   }
 
-  get inactiveProjects() {
-    return projectData.filter(
-      project => project.name !== this.props.currentProject.name
-    );
-  }
-
-  formatProjectName(name) {
-    if (name === 'tmmnews') {
-      return name.slice(0, 3).toUpperCase() + name.slice(3);
-    } else {
-      return name.slice(0, 1).toUpperCase() + name.slice(1);
-    }
+  get projectDataForDisplay() {
+    return this.props.currentProject
+      ? projectData.filter(
+        project => project.name !== this.props.currentProject.name
+      )
+      : projectData;
   }
 
   render() {
-    return this.inactiveProjects.map((project, index) => (
+    return this.projectDataForDisplay.map((project, index) => (
       <section id="nav-group" key={index}>
         <h1>
-          {this.formatProjectName(project.name)} | {project.details.type}
+          {formatProjectName(project.name)} | {project.details.type}
         </h1>
         <SingleProjectNav projectData={project} />
       </section>
--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -14,7 +14,16 @@ function getPath(props) {
   return props.location.pathname.toLowerCase();
 }
 
+function formatProjectName(name) {
+  if (name === 'tmmnews') {
+    return name.slice(0, 3).toUpperCase() + name.slice(3);
+  } else {
+    return name.slice(0, 1).toUpperCase() + name.slice(1);
+  }
+}
+
 module.exports = {
   normalize,
-  getPath
+  getPath,
+  formatProjectName
 };
