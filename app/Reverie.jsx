ef21302487d6e561a56b6d0172800187dbf5a96e
James Abels <abelsj60@gmail.com>
2018-11-11 13:11:56 -0500

    Feature: Dynamically import all reveries from ./reveries folder

--- app/Reverie.jsx
+++ app/Reverie.jsx
@@ -2,27 +2,33 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import ReactHtmlParser from 'react-html-parser';
 import marked from 'marked';
-import post from './reveries/11-10-18.md';
+import reveries from './reveries/index';
 
 class Reverie extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentReverie: reveries[0],
+      allReveries: reveries
+    };
   }
 
-  get date() {
-    return new Date(Date.now('en-us')).toLocaleString();
+  get reverie() {
+    return this.state.currentReverie;
   }
 
   render() {
-    // console.log(post);
+    // console.log('Post:', this.reverie);
+    // console.log('Post[0]:', reverie[0]);
     return (
       <main id="reverie">
         <section id="reverie-unused" className="left" />
         <section id="reverie-content" className="right">
           <h1>Reverie</h1>
-          <h2>{post.attributes.headline}</h2>
-          <p>{post.attributes.date}</p>
-          {ReactHtmlParser(marked(post.body))}
+          <h2>{this.reverie.attributes.hed}</h2>
+          <p>{this.reverie.attributes.date}</p>
+          {ReactHtmlParser(marked(this.reverie.body))}
         </section>
       </main>
     );
