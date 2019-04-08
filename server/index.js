552101a10425ccb65d49c959ef4d89552b871e1b
James Abels <abelsj60@gmail.com>
2019-04-08 12:41:00 -0400

    Scripts added to head, logs removed, comments removed

--- server/index.js
+++ server/index.js
@@ -5,15 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = 3300;
 
-// 1. gh-pages — index.html <script src='build/main.[hash].js />
-// a. I can't set the static folder, so no absolute path in src='',
-// Use a relative path to the folder and the file...
-// b. Could add a '/public' (rel, no slash) folder <link src='public/main...' />
-
-// 2. localhost — index.html <script src='/main.[hash].js' />
-// a. I can set the static folder, so can do a short absolute path of a single '/'.
-// In other words, don't use a '/build' folder in path b/c it's included in express.static
-// c. Same with /public items
+// 1. gh-pages — index.html <script src='/build/main.[hash].js />
 
 app.use(volleyball);
 app.use(express.static(path.join(__dirname, '../public')));
