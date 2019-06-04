86c6e9a93b77a85859ff0cf230b3a4525a91c3d0
James Abels <abelsj60@gmail.com>
2019-06-04 12:35:07 -0400

    changed motto margin

--- app/home/NameTag.jsx
+++ app/home/NameTag.jsx
@@ -50,7 +50,7 @@ const Motto = styled.p`
   margin-left: 16px;
   
   @media (min-width: ${p => p.theme.mediaQueries.tinyView}) {
-    margin-left: 18px;
+    margin-left: 15px;
     font-size: ${p => !p.isCasting ? p.theme.fontSizes.eighteen : p.theme.fontSizes.eighteen};
   }
 `;
