eab54f124d9e21cdae0d2596af4234e7b45335b7
James Abels <abelsj60@gmail.com>
2019-06-04 12:45:17 -0400

    moved motto left

--- app/home/NameTag.jsx
+++ app/home/NameTag.jsx
@@ -50,7 +50,7 @@ const Motto = styled.p`
   margin-left: 16px;
   
   @media (min-width: ${p => p.theme.mediaQueries.tinyView}) {
-    margin-left: 15px;
+    margin-left: 10px;
     font-size: ${p => !p.isCasting ? p.theme.fontSizes.eighteen : p.theme.fontSizes.eighteen};
   }
 `;
