537d57e989098160c53ee167e3f00125e8c0b102
James Abels <abelsj60@gmail.com>
2019-11-17 17:08:05 -0500

    made links conditionally yellow

--- app/header-footer/Header.jsx
+++ app/header-footer/Header.jsx
@@ -89,13 +89,18 @@ const RestyledLink = styled(
     font-size: ${p => p.isHome ? p.theme.fontSizes.three : p.theme.fontSizes.three};
   }
 
-  // Control nav items when menu is open, up to the break point
+  // Control nav items when menu is open, up to the break point, 1
   @media (min-width: 0px) and (max-width: ${p => p.theme.mediaQueries.tinyView}) {
+    color: ${p => p.useAref && p.theme.colors.yellow};
+    font-family: ${p => p.useAref && "'Aref Ruqaa', serif"};
     font-size: ${p => !p.nameAsLink && p.tempContent === 3 && p.theme.fontSizes.eighteen};
     margin-left: ${p => !p.nameAsLink && p.tempContent === 3 && '0px'};
   }
 
+  // Control nav items when menu is open, up to the break point, 2
   @media (min-width: ${p => p.theme.mediaQueries.tinyView}) and (max-width: ${p => p.theme.mediaQueries.narrowBreakTwo}) {
+    color: ${p => p.useAref && p.theme.colors.yellow};
+    font-family: ${p => p.useAref && "'Aref Ruqaa', serif"};
     font-size: ${p => !p.nameAsLink && p.tempContent === 3 && p.theme.fontSizes.twenty};
     margin-left: ${p => !p.nameAsLink && p.tempContent === 3 && '0px'};
   }
@@ -293,6 +298,7 @@ export default class Header extends Component {
                       tempContent={tempContent}
                     >
                       <RestyledLink
+                        useAref={true}
                         boundHandleClickForApp={boundHandleClickForApp}
                         illustrationDirection={illustrationDirection}
                         illustrationLevel={illustrationLevel}
